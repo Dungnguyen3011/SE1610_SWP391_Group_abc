ef1f744fca91cfd9bfffb7fae7504a7026cbefeb
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-09-16 15:22:38 +0700

    Initial commit

---
+++ src/main/java/com/swp391/Ebutler/EbutlerApplication.java
@@ -0,0 +1,13 @@
+package com.swp391.Ebutler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbutlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbutlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swp391/Ebutler/EbutlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swp391.Ebutler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbutlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
