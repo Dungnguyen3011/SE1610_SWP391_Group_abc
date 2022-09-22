cb3b18a165238adf923162c4f9e1f2b51189476d
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-09-22 21:29:42 +0700

    set up entity Account

--- src/main/java/com/swp391/ebutler/entities/Account.java
+++ src/main/java/com/swp391/ebutler/entities/Account.java
@@ -1,5 +1,30 @@
 package com.swp391.ebutler.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Table(name="tbl_account")
+@Entity
+@Data
 public class Account {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="account_id", nullable = false, updatable = false)
+	private Integer accountId;
+	
+	@Column(name="login_mail")
+	private String loginMail;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="status")
+	private Boolean status;
 }
