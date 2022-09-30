7f1b892c5b9c45c87f2e78c7ddee178aca1a5af8
blvm159263 <builevanminh0809@gmail.com>
2022-09-30 11:02:15 +0700

    Add validation for DTO

--- src/main/java/com/swp391/ebutler/controllers/ManufacturerAPI.java
+++ src/main/java/com/swp391/ebutler/controllers/ManufacturerAPI.java
@@ -3,6 +3,7 @@ package com.swp391.ebutler.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +28,11 @@ public class ManufacturerAPI {
 	@GetMapping("/list")
 	public ResponseEntity<?> getList(){
 		List<ManufacturerDTO> result = manuService.listAll();
+		return ResponseEntity.ok(result);
+	}
+	
+	public ResponseEntity<?> getListByName(@Param("name") String name){
+		List<ManufacturerDTO> result = manuService.searchByName(name);
 		return ResponseEntity.ok(result);
 	}
 	
