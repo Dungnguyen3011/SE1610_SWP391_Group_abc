c82cbf539548127b4dbed489945ba18249636d82
Dungnguyen3011 <84926618+Dungnguyen3011@users.noreply.github.com>
2022-10-11 20:19:10 +0700

    Update ServicesRepository.java

--- src/main/java/com/swp391/ebutler/repositories/ServicesRepository.java
+++ src/main/java/com/swp391/ebutler/repositories/ServicesRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import com.swp391.ebutler.entities.ServiceCategory;
 import com.swp391.ebutler.entities.Services;
 
 public interface ServicesRepository extends JpaRepository<Services, Integer> {
