beaa9d909ea3bc98b88e53d620bc8d30f7cfa577
blvm159263 <builevanminh0809@gmail.com>
2022-10-13 00:29:16 +0700

    add update pProvider

--- src/main/java/com/swp391/ebutler/controllers/ProductProviderAPI.java
+++ src/main/java/com/swp391/ebutler/controllers/ProductProviderAPI.java
@@ -5,16 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.swp391.ebutler.model.dto.ProductProviderDTO;
 import com.swp391.ebutler.service.ProductProviderService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/pprovider")
@@ -63,6 +59,12 @@ public class ProductProviderAPI {
 	@DeleteMapping("/list/{id}")
 	public ResponseEntity<?> delete(@PathVariable("id") Integer id){
 		return ResponseEntity.ok(pproviderService.delete(id));
+	}
+
+	@PutMapping("/list/{id}")
+	public ResponseEntity<?> update(@PathVariable("id") Integer id ,@Valid @RequestBody ProductProviderDTO pProvider){
+		pProvider.setProvider_id(id);
+		return ResponseEntity.ok(pproviderService.save(pProvider));
 	}
 	
 	//List product provider by ManuId
