0011d495a579aabb0548d252d55eeee12a0e8cfa
LeBuoi <nickbui0301@gmail.com>
2022-10-18 00:27:17 +0700

    Update AccountRepository.java

--- src/main/java/com/swp391/ebutler/repositories/AccountRepository.java
+++ src/main/java/com/swp391/ebutler/repositories/AccountRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.swp391.ebutler.entities.Account;
 
 public interface AccountRepository extends JpaRepository<Account, Integer> {
+    
+    Account findByLoginMailAndPassword(String loginMail, String password);
 
 }
