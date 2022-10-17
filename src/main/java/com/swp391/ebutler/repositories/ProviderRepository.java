373254d6f43124dcb807c26b3c54e0be7f73c11b
LeBuoi <nickbui0301@gmail.com>
2022-10-18 00:26:33 +0700

    Update ProviderRepository.java

--- src/main/java/com/swp391/ebutler/repositories/ProviderRepository.java
+++ src/main/java/com/swp391/ebutler/repositories/ProviderRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.swp391.ebutler.entities.Provider;
 
 public interface ProviderRepository extends JpaRepository<Provider, Integer>{
-
+    
+@Query("SELECT p FROM Provider p Where p.account.accountId= :id")
+	Provider findByAccountId(Integer id);
 }
